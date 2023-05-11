223f439e5394632c9c1923999e2d1cd3da979472
realchayka <lonelyplay54@gmail.com>
2023-05-11 12:49:10 +0300

    Added Navbar.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
-import { useTheme } from './providers/ThemeProvider';
+import {useTheme} from './providers/ThemeProvider';
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-           <AppRouter />
+            <AppRouter/>
         </div>
     );
 };
